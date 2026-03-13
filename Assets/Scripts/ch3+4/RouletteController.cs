a6cf35d089c5d23ac2b8fa879cbcc1f4c5715866
shota67-git <shota67@taivs.tp.edu.tw>
2026-03-13 10:52:48 +0800

    0313v1

--- Assets/Scripts/ch3+4/RouletteController.cs
+++ Assets/Scripts/ch3+4/RouletteController.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class RouletteController : MonoBehaviour
 {
     float rotSpeed = 0;
+    float rotangle;
+    public TextMeshProUGUI show;
+    public TextMeshProUGUI record;
+    private void Start()
+    {
+        rotangle = transform.eulerAngles.z;
+    }
     private void Update()
     {
         if (Input.GetMouseButtonDown(0)) {
             rotSpeed = 100;
         }
         transform.Rotate(0, 0, rotSpeed);
-        rotSpeed *= 0.98f;
-        Debug.Log(transform.eulerAngles.z);
+        if(rotSpeed>0.01f)
+            rotSpeed *= 0.98f;
+        else
+            rotSpeed = 0;
+        if (rotSpeed == 0)
+        {
+            if (rotangle >= -30 && rotangle <= 30) show.text="¥û";
+            
+        }
+
+        
     }
 }
